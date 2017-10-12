237d8bf5353e11804b8d5b3afacfc1ed24863adf
Yoan Eynaud <yoan.eynaud@gmail.com>
2017-10-12 14:43:27 -0700

    first full devp

--- R/IndPatchSize.R
+++ R/IndPatchSize.R
@@ -1,49 +1,60 @@
-#' A function to create fake Length/Age relationship for fish
+#' Patch size distribution extractor
 #'
-#' This function allows you create fake data, with added noise.
-#' @param n Number of fish captured
-#' @param maxA Age of the oldest fish captured (years)
-#' @param K  Mean growth rate (mm/years)
-#' @param Linf  Mean maximum size (mm)
-#' @param noiseK  Standard deviation of the growth rate (mm/years)
-#' @param noiseL  Standard deviation of the maximum size (mm)
-#' @return A dataframe containing the simulated Age(year) and Length(mm) along with a plot presenting Length(mm) as a function of age(year).
-#' @keywords Fish, Von Bertalanffy, simulation
+#' This function allows you to extract the size and type of all the patches present in an image
+#' @param X the path of the file to analyses
+#' @param Y the path of the folder containing the named pic which unique color fitting a specific type of organism potentially present on the plot
+#' @param Z a chain of character to name the *.csv and *.jpg files
+#' @return A dataframe (both in R and as a *.csv in the working directory) containing the type and size of each patch present on the analyzed image, also prints a picture with the labeled patches (same resolution as original)
+#' @keywords Mosaic, image analysis, patch, size
 #' @export
-#' @examples
-#' fake_fish_creator(12,32,0,01,10,0.5,1200)
+#' @examples #working on it
 
 
-require(jpeg)
-require(png)
-require(raster)
-require(foreach)
+IndPatchSize=function(X,Y,Z){
+  require(jpeg)
+  require(png)
+  require(raster)
+  require(foreach)
 
-# X is the path of the file to analyses
-# Y is the path of the legend
+  X='/Users/yoaneynaud/Desktop/Travail/Post_doc_scripps/Mosaic/test_for_package/MAI_2016_cropped.png'
+  Y='/Users/yoaneynaud/Desktop/Travail/Post_doc_scripps/Mosaic/test_for_package/Legend/'
+  Z='/Users/yoaneynaud/Desktop/Travail/Post_doc_scripps/Mosaic/test_for_package/RESULT'
+
 
 
   list_species=dir(Y,pattern='png')
-  species=foreach(i=1:length(list_species),.combine=rbind)%do%{
-    profile=readPNG(paste(Y,list_species[i],sep=''))
-    profile[1,1,1:3]
-  }
-  colnames(species)=c('red','green','blue')
-  rownames(species)=unlist(strsplit(list_species,'.png'))
-
+  if(length(list_species)==1){
+    species=t(as.data.frame(readPNG(paste(Y,list_species,sep=''))[1,1,1:3]))
+    colnames(species)=c('red','green','blue')
+    rownames(species)=unlist(strsplit(list_species,'.png'))
+  }else{
+    species=foreach(i=1:length(list_species),.combine=rbind)%do%{
+      profile=readPNG(paste(Y,list_species[i],sep=''))
+      profile[1,1,1:3]
+    }
+    colnames(species)=c('red','green','blue')
+    rownames(species)=unlist(strsplit(list_species,'.png'))}
+  cat('I will be able to identify the following groups: ',paste(rownames(species),collapse=', '),fill=T)
   # analysis
 
-    masterfile=readPNG(paste('to_be_analyzed/',to_analyze[i],sep=''))
-    cat(paste('I am currenlty working on this mosaic: ',to_analyze[i],sep=''),fill=T)
-    step=1
-    quad_data=foreach(ii=1:nrow(species),.combine=rbind)%do%{
-      cat(paste('I am currenlty working on this group: ',rownames(species)[ii],sep=''),fill=T)
-      if(ii==1){base_num=0}else{base_num=base_num+1}
-      extract=matrix(0,dim(masterfile)[1],dim(masterfile)[2])   # IS THIS USELESS ?
-      adonde_coral=which(masterfile[,,1]==species[ii,1]&masterfile[,,2]==species[ii,2]&masterfile[,,3]==species[ii,3])
-      if(length(adonde_coral)>1){
+  masterfile=readPNG(X)
+  cat(paste('I am currenlty working on this image: ',X,sep=''),fill=T)
+  step=1
+  base_num=0
+  quad_data=data.frame('Group'=NA,'patch_ID'=NA,'x'=NA,'y'=NA,'area_in_pix_num'=NA)
+  #quad_data=foreach(ii=1:nrow(species),.combine=rbind)%do%{
+  for(ii in 1:nrow(species)){
+    cat(paste('I am currenlty working on this group: ',rownames(species)[ii],sep=''),fill=T)
+    base_num=base_num+1
+    extract=matrix(0,dim(masterfile)[1],dim(masterfile)[2])   # IS THIS USELESS ?
+    adonde_coral=which(masterfile[,,1]==species[ii,1]&masterfile[,,2]==species[ii,2]&masterfile[,,3]==species[ii,3])
+    if(length(adonde_coral)>1){
       extract[adonde_coral]=1    # IS THIS USELESS ?
-      named_layer=as.matrix(clump(raster(extract)))
+      AA=raster(extract)
+      BB=clump(AA)
+      named_layer=t(matrix(BB,ncol(BB),nrow(BB)))
+      remove(AA,BB)
+      gc()
       named_layer=named_layer+base_num
       colony_names=unique(array(named_layer))
       colony_names=colony_names[!is.na(colony_names)]
@@ -51,14 +62,16 @@ require(foreach)
       Width=ncol(named_layer)
       if(step==1){test=named_layer}else{
         test=test
-      step=step+1
+        step=step+1
       }
       test[!is.na(named_layer)]=0.8
       named_layer[is.na(named_layer)]=0
 
+      if(dir.exists('name/')==FALSE)  dir.create('name/')
       # stamping picture process
+      pb <- txtProgressBar(min = 0, max = length(colony_names), style = 3)
       for(stamp in 1:length(colony_names)){
-
+        setTxtProgressBar(pb, stamp)
         #stamping the picture
         png(file=paste('name/',colony_names[stamp],'.png',sep=''),width = 500, height = 250)
         plot(1,1,axes=F,'n',xaxt = "n" ,yaxt = "n",xlab='',ylab='')
@@ -84,26 +97,32 @@ require(foreach)
 
         test[(line_max-dim(ID)[1]+1):line_max,(col_max-dim(ID)[2]+1):col_max]=ID[,,1]
       }
-      #if(length(colony_names)>1){
+      close(pb)
+      unlink('name/', recursive = TRUE)
+
       species_result=as.data.frame(foreach(j=1:length(colony_names),.combine=rbind)%do%{
         pos_2D=as.numeric(round(apply(which(named_layer==colony_names[j],arr.ind=T),2,mean)))
         cbind(rownames(species)[ii],colony_names[j],pos_2D[2],pos_2D[1],length(which(array(extract[which(named_layer==colony_names[j])])==1)))
       })
-      colnames(species_result)=c('Group','patch_ID','x','y',' area in # pix')
+      colnames(species_result)=c('Group','patch_ID','x','y','area_in_pix_num')
       rownames(species_result)=NULL
       base_num=max(colony_names)
-      species_result
+      quad_data=rbind(quad_data,species_result)
     }
-    }
-    writeJPEG(test,paste('analyzed/',unlist(strsplit(to_analyze[i],'.png')),'_identified.jpeg',sep=''))
 
-    #mat_dist=as.matrix(dist(cbind(as.numeric(as.character(quad_data$x)),as.numeric(as.character(quad_data$y)))))
-    #colnames(mat_dist)=quad_data$patch_ID
-    #diag(mat_dist)=NA
-    #quad_data=cbind(quad_data,mat_dist)
-    write.csv(quad_data,paste('analyzed/',unlist(strsplit(to_analyze[i],'.png')),'_analysis.csv',sep=''))
-    #file.copy(from, to, overwrite = recursive, recursive = FALSE,copy.mode = TRUE, copy.date = FALSE)
+  }
 
+  quad_data=quad_data[-1,]
+  writeJPEG(test,paste(Z,'_with_ID.jpeg',sep=''))
+
+  #mat_dist=as.matrix(dist(cbind(as.numeric(as.character(quad_data$x)),as.numeric(as.character(quad_data$y)))))
+  #colnames(mat_dist)=quad_data$patch_ID
+  #diag(mat_dist)=NA
+  #quad_data=cbind(quad_data,mat_dist)
+  write.csv(quad_data,paste(Z,'_analysis_output.csv',sep=''))
+  #file.copy(from, to, overwrite = recursive, recursive = FALSE,copy.mode = TRUE, copy.date = FALSE)
+  return(quad_data)
+}
 
 
 
