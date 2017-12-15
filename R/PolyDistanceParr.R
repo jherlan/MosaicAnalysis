49adfcf049635b759391a86308b74564fce699fe
Yoan Eynaud <yoan.eynaud@gmail.com>
2017-12-15 15:32:24 -0800

    update

--- R/PolyDistanceParr.R
+++ R/PolyDistanceParr.R
@@ -1,3 +1,21 @@
+#' Distance bewteen polygons
+#'
+#' None of what follows make sense
+#' @param X wether (i) the path of the file to analyse, (ii) a matrix where each cell contains a number representing the cell type (as outputed by the function \code{FromPictoRdata}) or (iii) a SpatialPolygonDataFrame.
+#' @param Y if \code{X} is the path of the file to analyse, \code{Y} is the path of the folder containing the named pic which unique color fitting a specific type of organism potentially present on the plot. If \code{Y=NA}, then \code{X} is considered being a matrix where each cell contains a number representing the cell type (as outputed by the function \code{FromPictoRdata}).
+#' @param Z a chain of character to name the *.csv and *.jpg files (defaults to \code{X})
+#' @return A dataframe (both in R and as a *.csv in the working directory) containing the type and size of each patch present on the analyzed image, also prints a picture with the labeled patches (same resolution as original)
+#' @keywords Mosaic, image analysis, patch, size
+#' @export
+#' @examples #working on it
+
+#X='/Users/yoaneynaud/Desktop/Travail/Post_doc_scripps/Mosaic/test_for_package/MAI_2016.png'
+#Y='/Users/yoaneynaud/Desktop/Travail/Post_doc_scripps/Mosaic/test_for_package/Legend/'
+
+
+
+
+
 PolyDistanceParr=function(X,Y=NULL,cores=(detectCores()/2)){
   if(is.null(Y)){
     cat(paste('I am using here ',cores,' cores',sep=''),fill=T)
