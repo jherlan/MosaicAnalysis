a76f9d2042d45701dc37f3acd1eaf4cd75385b10
Yoan Eynaud <yoan.eynaud@gmail.com>
2017-12-15 16:34:09 -0800

    adding a progress bar

--- R/PolyDistanceParr.R
+++ R/PolyDistanceParr.R
@@ -25,9 +25,12 @@ PolyDistanceParr=function(X,cores=(detectCores()/2)){
       cores=makeCluster(cores)
       registerDoParallel(cores)
       eloign=matrix(0,length(X@polygons),length(X@polygons))
+      pb <- txtProgressBar(min = 1, max = (length(X@polygons)-1), style = 3)
       for(j in 1:(length(X@polygons)-1)){
+        setTxtProgressBar(pb,j)
         eloign[j,(j+1):length(X@polygons)]=foreach(i=(j+1):length(X@polygons),.combine=c,.packages='pdist')%dopar%{min(pdist(X@polygons[[j]]@Polygons[[1]]@coords,X@polygons[[i]]@Polygons[[1]]@coords)@dist)}
       }
+      close(pb)
       eloign=eloign+t(eloign)
       eloign[which(eloign==0)]=NA
       stopCluster(cores)
